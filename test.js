83047adb2232a2b52275fb4d735b8cabc7aa8bb1
Alexander Kulikov <kulik_alex_91@mail.ru>
2021-12-01 18:38:43 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,38 @@
 const fs = require('fs');
+const stylelint = require('stylelint');
 
-try {
-    const gitIgn = fs.readFileSync('.gitignore').toString();
-} catch (excep) {
-    console.error(`Файл \`${'.gitignore'}\` не создан.`);
+const stylelint_opt = ()=>{
+    return JSON.parse(fs.readFileSync(path.join(__dirname, 'stylelint.json'), 'utf-8').toString());
 }
+
+async function useLint() {
+    const data = await stylelint.lint({
+        files: projectPath + '**/*.css',
+        config: stylelint_opt()
+    });
+    const errors = [];
+    for (let result of data.results) {
+        for (let warning of result.warnings) {
+            console.error(String(Object.keys(warning)));
+        }
+    }
+}
+
+async function test() {
+    try {
+        const gitIgn = fs.readFileSync('.gitignore').toString();
+    } catch (excep) {
+        console.error(`Файл \`${'.gitignore'}\` не создан.`);
+    }
+
+    try {
+        const rm = fs.readFileSync('README.md').toString();
+    } catch (excep) {
+        console.error(`Файл \`${'README.md'}\` не создан.`);
+    }
+    await useLint();
+}
+
+test().then(errors=>{}
+).catch(x=>{}
+);
